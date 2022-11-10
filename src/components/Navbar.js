712612e7db5482b05387dc96ff57ea29fc147e5e
saM041988 <sambitmohanty041988@gmail.com>
2022-11-10 20:23:16 +0530

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="/">
+                Redux First App
+            </a>
+            <button
+                className="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span className="navbar-toggler-icon" />
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto">
+                <li className="nav-item active">
+                    <a className="nav-link" href="/">
+                    Home <span className="sr-only">(current)</span>
+                    </a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="/">
+                    About
+                    </a>
+                </li>
+                
+                </ul>
+                <div>
+                    <button className='btn btn-primary'>Your Count 0</button>
+                </div>
+            </div>
+            </nav>
+    </>
+  )
+}
+
+export default Navbar
---
+++ src/components/Shop.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <>
+        <div className='text-center my-5'>
+            <h4 className='mb-4'>Buy Samsung TV Rs. 13500</h4>
+            <button className='btn btn-primary mr-3'>-</button>
+            Add to cart
+            <button className='btn btn-primary ml-3'>+</button>
+        </div>
+        
+    </>
+  )
+}
+
+export default Shop
